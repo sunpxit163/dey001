e802fe70d46f0e3e2b29fc060bf93045555e3349
spx <sunpxit@126.com>
2019-09-04 16:26:21 +0800

    自定义算法

--- client_80/src/main/java/xyz/sunpxit/config/ConfigBean.java
+++ client_80/src/main/java/xyz/sunpxit/config/ConfigBean.java
@@ -1,12 +1,14 @@
 package xyz.sunpxit.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import xyz.myrule.MySelfRule;
 
 @Configuration
-
+@RibbonClient(name = "MICROSERVICECLOUD-DEPT8001",configuration = MySelfRule.class)
 public class ConfigBean {
     @Bean
     @LoadBalanced
